0c8abcec811c3c11c17b93e4ae08baeddb0853b9
Gregory Kan <kan.gregory@gmail.com>
2015-05-06 11:56:47 +1200

    created new static class that starts async thread for twitter stream

--- glassteeth/glassteeth/Controllers/StreamController.cs
+++ glassteeth/glassteeth/Controllers/StreamController.cs
@@ -15,32 +15,8 @@ namespace glassteeth.Controllers
     {
         public void GetMyStream(string input)
         {
-            TwitterAuth twitterAuth = new TwitterAuth();
-            PusherStream stream = new PusherStream();
-            SentimentAnalysis SA = new SentimentAnalysis();
-
-            stream.Pusher.Trigger("tweetStream", "tweetEvent", new {message = "controller method hit"});
-
-            var filteredStream = Stream.CreateFilteredStream();
-            filteredStream.AddTrack(input);
-            filteredStream.MatchingTweetReceived += (sender, arg) =>
-            {
-                stream.Pusher.Trigger("tweetStream", "tweetEvent", new { message = "this is a tweet" });
-                //if (arg.Tweet.Coordinates != null)
-                //{
-                //    string sentiment = SA.Analyze(arg.Tweet.Text);
-                //    MyTweet thisTweet = new MyTweet(arg.Tweet.Text,
-                //        arg.Tweet.Coordinates.Latitude.ToString(), arg.Tweet.Coordinates.Longitude.ToString(), sentiment);
-                //    stream.Pusher.Trigger("tweetStream", "tweetEvent", new {message = thisTweet});
-                //}
-                //else if (arg.Tweet.Place != null)
-                //{
-                //    string sentiment = SA.Analyze(arg.Tweet.Text);
-                //    MyTweet thisTweet = new MyTweet(arg.Tweet.Text, arg.Tweet.Place.Name, sentiment);
-                //    stream.Pusher.Trigger("tweetStream", "tweetEventWithPlace", new {message = thisTweet});
-                //}
-            };
-            filteredStream.StartStreamMatchingAllConditions();
+            PusherStream.Term = input;
+            PusherStream.StartAsyncTask();
         }
     }
 }
--- glassteeth/glassteeth/Models/PusherStream.cs
+++ glassteeth/glassteeth/Models/PusherStream.cs
@@ -4,19 +4,47 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using PusherServer;
+using System.Threading.Tasks;
+using Tweetinvi;
 
 namespace glassteeth.Models
 {
-    public class PusherStream
+    public static class PusherStream
     {
-        private string pusherAppId = "117600";
-        private string pusherAppKey = "45c06fa98717fe603c5a";
-        private string pusherAppSecret = "62f457d7131dbb7708b4";
-        public Pusher Pusher;
+        public static string Term { get; set; }
 
-        public PusherStream()
+        public static void StartAsyncTask()
         {
-            Pusher = new Pusher(pusherAppId, pusherAppKey, pusherAppSecret);
+            Task.Factory.StartNew(TweetStream);
+        }
+
+        public static void TweetStream()
+        {
+            TwitterAuth twitAuth = new TwitterAuth();
+            Pusher pusher = new Pusher("117600", "45c06fa98717fe603c5a", "62f457d7131dbb7708b4");
+            SentimentAnalysis SA = new SentimentAnalysis();
+
+            var filteredStream = Stream.CreateFilteredStream();
+            filteredStream.AddTrack(Term);
+            filteredStream.MatchingTweetReceived += (sender, arg) =>
+            {
+                pusher.Trigger("tweetStream", "tweetEvent", new { message = arg.Tweet.Text });
+                //if (arg.Tweet.Coordinates != null)
+                //{
+                //    string sentiment = SA.Analyze(arg.Tweet.Text);
+                //    MyTweet thisTweet = new MyTweet(arg.Tweet.Text,
+                //        arg.Tweet.Coordinates.Latitude.ToString(), arg.Tweet.Coordinates.Longitude.ToString(), sentiment);
+                //    stream.Pusher.Trigger("tweetStream", "tweetEvent", new {message = thisTweet});
+                //}
+                //else if (arg.Tweet.Place != null)
+                //{
+                //    string sentiment = SA.Analyze(arg.Tweet.Text);
+                //    MyTweet thisTweet = new MyTweet(arg.Tweet.Text, arg.Tweet.Place.Name, sentiment);
+                //    stream.Pusher.Trigger("tweetStream", "tweetEventWithPlace", new {message = thisTweet});
+                //}
+            };
+            filteredStream.StartStreamMatchingAllConditions();
+
         }
     }
 }
