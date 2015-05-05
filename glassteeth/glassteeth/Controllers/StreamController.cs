0caa04078618d2d88b831433ffdd589b5b381e6d
gregorykan <kan.gregory@gmail.com>
2015-05-05 16:55:19 +1200

    added pusher nuget and controller

---
+++ glassteeth/glassteeth/Controllers/StreamController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using glassteeth.Models;
+
+namespace glassteeth.Controllers
+{
+    public class StreamController : ApiController
+    {
+        public string GetMyStream(string input)
+        {
+            PusherStream stream = new PusherStream();
+            return "hello";
+        }
+    }
+}
--- glassteeth/glassteeth/Models/PusherStream.cs
+++ glassteeth/glassteeth/Models/PusherStream.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using PusherServer;
 
 namespace glassteeth.Models
 {
     public class PusherStream
     {
+        private string pusherAppId = "117600";
+        private string pusherAppKey = "45c06fa98717fe603c5a";
+        private string pusherAppSecret = "62f457d7131dbb7708b4";
 
+        public PusherStream()
+        {
+            var pusher = new Pusher(pusherAppId, pusherAppKey, pusherAppSecret);
+        }
     }
 }
